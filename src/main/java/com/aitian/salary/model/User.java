8d574ba60572cdc374a6f22b40ea4564b272b24e
kanmomo <1096187609@qq.com>
2018-05-29 23:03:56 +0800

    登录

--- src/main/java/com/aitian/salary/model/User.java
+++ src/main/java/com/aitian/salary/model/User.java
@@ -1,5 +1,30 @@
 package com.aitian.salary.model;
 
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+@Table(name = "s_user")
 public class User {
 
+    @Column(name = "empid")
+    private String empId;
+
+    @Column(name = "password")
+    private String password;
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
