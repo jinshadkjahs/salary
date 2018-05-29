7acdde3dcbddb0eca67085f4c15e9611e05696f9
kanmomo <1096187609@qq.com>
2018-05-30 02:02:01 +0800

    live Reload

--- src/main/java/com/aitian/salary/controller/IndexController.java
+++ src/main/java/com/aitian/salary/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.aitian.salary.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @Author yisicheng 2018-05-29
  * 静态页面跳转
@@ -14,7 +16,13 @@ public class IndexController {
      *
      */
     @RequestMapping("/index")
-    public String helloHtml() {
+    public String index() {
         return "/index";
     }
+
+    @RequestMapping("/pages/*")
+    public String main(HttpServletRequest request) {
+        return request.getRequestURI();
+    }
+
 }
