0845ad9f315e2693a794e91c6ca5420a50f24cbd
kanmomo <1096187609@qq.com>
2018-05-29 23:30:02 +0800

    login message update

--- src/main/java/com/aitian/salary/controller/UserController.java
+++ src/main/java/com/aitian/salary/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.aitian.salary.controller;
 
+import com.aitian.salary.Utils.ConverterSystem;
 import com.aitian.salary.model.User;
 import com.aitian.salary.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
@@ -18,11 +23,16 @@ public class UserController {
 
     @RequestMapping("/login")
     @ResponseBody
-    public User toIndex(HttpServletRequest request, Model model){
+    public String toIndex(HttpServletRequest request, Model model) throws JsonProcessingException {
         String empId = request.getParameter("id");
         String pwd = request.getParameter("pwd");
         User user = this.userService.findUserByLogin(empId, pwd);
-        return user;
+        if(user != null){
+            request.getSession().setAttribute(ConverterSystem.SESSION_USER_KEY,user);
+        }
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("loginInfo",user!=null);
+        return new ObjectMapper().writeValueAsString(map);
     }
 
 }
