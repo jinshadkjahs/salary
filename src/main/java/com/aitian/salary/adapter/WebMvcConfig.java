aef6a6abc479ade1b7b8f80ed73d0332dcaa55d9
yisicheng <yisichengme@gmail.com>
2018-06-02 17:35:56 +0800

    页面调转

--- src/main/java/com/aitian/salary/adapter/WebMvcConfig.java
+++ src/main/java/com/aitian/salary/adapter/WebMvcConfig.java
@@ -3,13 +3,13 @@ package com.aitian.salary.adapter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
  * @Author yisicheng 2018-05-29
  */
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
