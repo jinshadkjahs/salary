d1884168a9739f304fa86f72000778c58f5d7f28
kanmomo <1096187609@qq.com>
2018-06-03 20:22:22 +0800

    员工管理--删除员工信息

--- src/main/java/com/aitian/salary/mapper/EmployeeMapper.java
+++ src/main/java/com/aitian/salary/mapper/EmployeeMapper.java
@@ -4,6 +4,7 @@ import com.aitian.salary.Utils.BaseMapper;
 import com.aitian.salary.mapper.provider.EmployeeDynaSqlProvider;
 import com.aitian.salary.model.Employee;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.SelectProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,26 +18,7 @@ import java.util.Map;
 @Mapper
 public interface EmployeeMapper extends BaseMapper<Employee> {
 
-//    @Autowired
-//    private JdbcTemplate jdbcTemplate;
-//    Logger logger = LoggerFactory.getLogger(this.getClass());
-//    public List<Map<String,Object>> findEmployee(String empId,String empName){
-//        StringBuffer sb = new StringBuffer();
-//        sb.append(" SELECT * FROM s_employee emp").append(" where 1=1");
-//                if(!StringUtils.isEmpty(empId)){
-//                    sb.append(" and emp.empid='"+empId+"'");
-//                }
-//                if(!StringUtils.isEmpty(empName)){
-//                    sb.append(" and emp.emp_name='"+empName+"'");
-//                }
-//        logger.info(sb.toString());
-//        return this.jdbcTemplate.queryForList(sb.toString());
-//    }
-
- /*   public List<Employee> insertEmployee();
-
-    public List<Employee> updateEmployee();
-
-    public List<Employee> delEmployee();*/
+    @SelectProvider(type = EmployeeDynaSqlProvider.class,method = "queryEmpAndUser")
+    List<Employee> queryEmpAndUser(Employee emp);
 
 }
