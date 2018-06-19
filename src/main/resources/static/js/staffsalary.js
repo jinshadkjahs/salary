771fdfc28e336e08b84bd40e3e62d9872b274923
yufei.zheng <yufei.zhneg@beebank.com>
2018-06-19 14:22:45 +0800

    合同工页面抬头调整

--- src/main/resources/static/js/staffsalary.js
+++ src/main/resources/static/js/staffsalary.js
@@ -49,9 +49,15 @@ function addTable(salaryDate) {
 
 function Generatdata(result, salaryDate) {
     var html = '';
-    var htmlHead = '<tr class="firstTr">' +
-        '<th width="10%" colspan="5" style="size:40px; height:60px;"><img src="../../static/img/logo.jpeg" width="50px" height="50px" align="center" style="padding-left: 20px;">&nbsp;&nbsp;&nbsp;' + salaryDate + '  山西医科大学第六医院职工工资明细表</th>' +
-        '</tr>';
+    if (result.emp_type == 0) {
+        var htmlHead = '<tr class="firstTr">' +
+            '<th width="10%" colspan="5" style="size:40px; height:60px;"><img src="../../static/img/logo.jpeg" width="50px" height="50px" align="center" style="padding-left: 20px;">&nbsp;&nbsp;&nbsp;' + salaryDate + ' 山西医科大学第六医院职工工资明细表</th>' +
+            '</tr>';
+    }else{
+        var htmlHead = '<tr class="firstTr">' +
+            '<th width="10%" colspan="5" style="size:40px; height:60px;"><img src="../../static/img/logo.jpeg" width="50px" height="50px" align="center" style="padding-left: 20px;">&nbsp;&nbsp;&nbsp;' + salaryDate + ' 山西医科大学第六医院合同制职工工资明细表</th>' +
+            '</tr>';
+    }
     html += '<tr>' +
         '<th width="20%">姓名</th>' +
         '<th width="20%" colspan="2">' + result.salaryInfo.employee.empName + '</th>' +
@@ -96,7 +102,7 @@ function Generatdata(result, salaryDate) {
         }
     }
 
-    if (result.emp_type = 0) {
+    if (result.emp_type == 0) {
         html += '<tr><th></th></tr><tr style="height: 40px;"><th width="20%">组成规则</th>' +
             '<th width="20%" colspan="4">工资额=保留工资\n</br>' +
             '职业=工令工资\n</br>' +
@@ -110,90 +116,3 @@ function Generatdata(result, salaryDate) {
         $("#usertable").html(htmlHead + html);
     }
 }
-
-//
-// function GeneratZSdata(result,salaryDate) {
-//     var html = '';
-//     var htmlHead = '<tr class="firstTr">\n' +
-//         '<th width="10%" colspan="5">****年**月医疗公司职工工资明细表</th>\n' +
-//         '</tr>';
-//     html += '<tr>\n' +
-//         '                        <th width="20%">姓名</th>\n' +
-//         '                        <th width="20%" colspan="2">张三</th>\n' +
-//         '                        <th width="20%">职工编号</th>\n' +
-//         '                        <th width="20%">HT789378939</th>\n' +
-//         '                    </tr>'+'<tr>\n' +
-//         '                        <th width="20%">身份号</th>\n' +
-//         '                        <th width="20%" colspan="2">149902198909097811</th>\n' +
-//         '                        <th width="20%">养老保险好</th>\n' +
-//         '                        <th width="20%">9378939</th>\n' +
-//         '                    </tr>'+'<tr>\n' +
-//         '                        <th width="20%" rowspan="4">固定资产</th>\n' +
-//         '                        <th width="20%">工龄工资</th>\n' +
-//         '                        <th width="20%">300.00</th>\n' +
-//         '                        <th width="20%">保留工资</th>\n' +
-//         '                        <th width="20%">120</th>\n' +
-//         '                    </tr>'+'<tr>\n' +
-//         '                        <th width="20%">各种补贴</th>\n' +
-//         '                        <th width="20%">300.00</th>\n' +
-//         '                        <th width="20%">回民</th>\n' +
-//         '                        <th width="20%">120</th>\n' +
-//         '                    </tr>'+'<tr>\n' +
-//         '                        <th width="20%">房补</th>\n' +
-//         '                        <th width="20%">300.00</th>\n' +
-//         '                        <th width="20%">专项奖</th>\n' +
-//         '                        <th width="20%">120</th>\n' +
-//         '                    </tr>'+'<tr>\n' +
-//         '                        <th width="20%">书报费</th>\n' +
-//         '                        <th width="20%">300.00</th>\n' +
-//         '                        <th width="20%">卫生费</th>\n' +
-//         '                        <th width="20%">120</th>\n' +
-//         '                    </tr>'+'<tr>\n' +
-//         '                        <th width="20%" rowspan="2">浮动工资</th>\n' +
-//         '                        <th width="20%">岗薪工资</th>\n' +
-//         '                        <th width="20%">300.00</th>\n' +
-//         '                        <th width="20%">绩效工资</th>\n' +
-//         '                        <th width="20%">120</th>\n' +
-//         '                    </tr>'+'<tr>\n' +
-//         '                        <th width="20%">节日加班</th>\n' +
-//         '                        <th width="20%">300.00</th>\n' +
-//         '                        <th width="20%">其他奖</th>\n' +
-//         '                        <th width="20%">120</th>\n' +
-//         '                    </tr>'+'<tr>\n' +
-//         '                        <th width="20%">其他</th>\n' +
-//         '                        <th width="20%">补发</th>\n' +
-//         '                        <th width="20%">300.00</th>\n' +
-//         '                        <th width="20%">扣款</th>\n' +
-//         '                        <th width="20%">120</th>\n' +
-//         '                    </tr>'+' <tr>\n' +
-//         '                        <th width="20%" rowspan="3">缴费部分</th>\n' +
-//         '                        <th width="20%">个税</th>\n' +
-//         '                        <th width="20%">300.00</th>\n' +
-//         '                        <th width="20%">养老</th>\n' +
-//         '                        <th width="20%">120</th>\n' +
-//         '                    </tr>'+'<tr>\n' +
-//         '                        <th width="20%">医疗</th>\n' +
-//         '                        <th width="20%">300.00</th>\n' +
-//         '                        <th width="20%">失业</th>\n' +
-//         '                        <th width="20%">120</th>\n' +
-//         '                    </tr>'+'<tr>\n' +
-//         '                        <th width="20%">公积金</th>\n' +
-//         '                        <th width="20%">300.00</th>\n' +
-//         '                        <th width="20%">其他</th>\n' +
-//         '                        <th width="20%">120</th>\n' +
-//         '                    </tr>'+'<tr>\n' +
-//         '                        <th width="20%">应领工资</th>\n' +
-//         '                        <th width="20%" colspan="2">3000.00</th>\n' +
-//         '                        <th width="20%">实领工资</th>\n' +
-//         '                        <th width="20%">3000.00</th>\n' +
-//         '                    </tr>'+'<tr>\n' +
-//         '                        <th width="20%">其他奖明细</th>\n' +
-//         '                        <th width="20%" colspan="4"></th>\n' +
-//         '                    </tr>'+' <tr>\n' +
-//         '                        <th width="20%">序号</th>\n' +
-//         '                        <th width="20%">金额</th>\n' +
-//         '                        <th width="20%" colspan="2">内容</th>\n' +
-//         '                        <th width="20%">归口管理部门</th>\n' +
-//         '                    </tr>';
-//     $("#usertable").html(htmlHead + html);
-// }
